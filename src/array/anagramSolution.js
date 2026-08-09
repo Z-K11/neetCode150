c65386a4ca00ebf02f6ef0588ddfa794ff3daf9f
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-09 12:18:57 +0500

    use direct string comparison instead of for loop

--- src/array/anagramSolution.js
+++ src/array/anagramSolution.js
@@ -1,9 +1,5 @@
 function isAnagram(s, t) {
-  s = s.split('').sort();
-  t = t.split('').sort();
-  if (s.length !== t.length) return false;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== t[i]) return false;
-  }
-  return true;
+  s = s.split('').sort().join('');
+  t = t.split('').sort().join('');
+  return s === t;
 }
