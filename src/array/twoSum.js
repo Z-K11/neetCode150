c7d94870060eee767d44e49b17c96471f93371f8
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-09 13:23:43 +0500

    sort files

--- src/array/twoSum.js
+++ src/array/twoSum.js
@@ -1,10 +1,3 @@
-export function twoSum(array, target) {
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] === target) return [i, j];
-    }
-    return undefined;
-  }
-}
+export function twoSum(array, target) {}
 
-console.log(twoSum([1, 2, 3], 4));
+//Do no write below this
---
+++ src/array/twoSumSolution.js
@@ -0,0 +1,8 @@
+function twoSum(array, target) {
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] + array[j] === target) return [i, j];
+    }
+    return undefined;
+  }
+}
